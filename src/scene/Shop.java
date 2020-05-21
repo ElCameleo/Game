3adb6ae6bc56d6ba89bf8b8f8e71a0d09a1186d4
Elred19 <65461188+Elred19@users.noreply.github.com>
2020-05-21 22:17:11 +0200

    Remodulation shop

--- src/scene/Shop.java
+++ src/scene/Shop.java
@@ -1,69 +1,102 @@
 package scene;
-
-import java.util.ArrayList;
-
+import item.potion.Bag;
+import item.potion.LifePotion;
+import item.potion.MegaLifePotion;
+import item.potion.PowerPotion;
 import item.Item;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.Labeled;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import main.Game;
+import mob.Dealer;
 
 public class Shop extends VBox {
-	
-	class Box extends VBox {
-		public Button btn;
-		public Box(String title) {
-			btn = new Button();
-			btn.setText(title);
-			btn.setFont(Font.font ("Verdana", 20));
-			btn.setAlignment(Pos.CENTER);
-			this.getChildren().addAll(btn);
-		}
-	}
-	
-
 	public Button quitBtn;
-	public HBox buttons;
-
+	Button LifePotionButton;
+	Button PowerPotionButton;
+	Button MegaLifePotionButton;
 	public Shop(double spacing) {
 		super(spacing);
 		
-		Label title = new Label("Shop du Turfu");
+		Label title = new Label("Bienvenue dans mon magasin !");
 		title.setFont(Font.font ("Verdana", 50));
-		title.setAlignment(Pos.TOP_CENTER);
-		
-		
-		
-		buttons = new HBox(4);
 		
 		quitBtn = new Button();
 		quitBtn.setText("Retourner au jeu");
 		quitBtn.setFont(Font.font ("Verdana", 20));
-		quitBtn.setAlignment(Pos.CENTER);
 		
-		buttons.getChildren().addAll(new Box("Object1"), new Box("Object2"), new Box("Object3"));
+		LifePotionButton = new Button("Potion de vie : Prix -> "+2+" | Nombre article restant -> "+5);
+		MegaLifePotionButton = new Button("Super potion de vie : Prix -> "+5+" | Nombre article restant -> "+5);
+		PowerPotionButton = new Button("Potion de force : Prix -> "+3+" | Nombre article restant -> "+5);
 		
-		this.getChildren().addAll(title, buttons, quitBtn);
+		this.setAlignment(Pos.CENTER);
+		this.getChildren().addAll(title, LifePotionButton,MegaLifePotionButton,PowerPotionButton,quitBtn);
 		
 	}
-
+	
+	
 	public void setGame(Game game) {
 		quitBtn.setOnAction((e) -> {
 			game.stage.setScene(Game.scene);
         });
 	}
 	
-	public void addGoodies(ArrayList<Item> items) {
-		int[] idx = { 0 };
-		buttons.getChildren().forEach((e) -> {
-			((Box) e).btn.setText(items.get(idx[0]).name);
-			idx[0]++;
+	
+	
+	public void printStore(Dealer d, Bag bag) {
+		int [] counter_list = d.itemCount(d.getList());
+		LifePotionButton.setOnAction(e -> {
+			if(e.getSource() == LifePotionButton) {
+				if(counter_list[0]>0) {
+					counter_list[0] = counter_list[0]-1;
+					LifePotionButton.setText("Potion de vie : Prix -> "+2+" | Nombre article restant -> "+counter_list[0]);
+				}
+				for(Item obj : d.getList()) {
+					if(obj instanceof LifePotion) {
+						bag.addPotion((LifePotion) obj);
+						d.getList().remove(obj);
+						break;
+					}
+				}
+			}
+		});
+		
+		MegaLifePotionButton.setOnAction(e -> {
+					
+			if(e.getSource() == MegaLifePotionButton) {
+				if(counter_list[1]>0) {
+					counter_list[1] = counter_list[1]-1;
+					MegaLifePotionButton.setText("Super potion de vie : Prix -> "+5+" | Nombre article restant -> "+counter_list[1]);	
+				}
+				for(Item obj : d.getList()) {
+					if(obj instanceof MegaLifePotion) {
+						bag.addPotion((MegaLifePotion) obj);
+						d.getList().remove(obj);
+						break;
+					}
+				}
+			}
+		});
+		
+		PowerPotionButton.setOnAction(e -> {
+					
+			if(e.getSource() == PowerPotionButton) {
+				if(counter_list[2]>0) {
+					counter_list[2] = counter_list[2]-1;
+					PowerPotionButton.setText("Potion de force : Prix -> "+8+" | Nombre article restant -> "+counter_list[2]);	
+				}
+				for(Item obj : d.getList()) {
+					if(obj instanceof PowerPotion) {
+						bag.addPotion((PowerPotion) obj);
+						d.getList().remove(obj);
+						break;
+					}
+				}
+			}
 		});
 	}
-
-
 }
+
+
