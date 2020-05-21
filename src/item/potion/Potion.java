aa780f002c14c736dd8d3063e25c4fe88f06c298
Elred19 <65461188+Elred19@users.noreply.github.com>
2020-05-21 22:30:11 +0200

    Ajout prix pour les potions

--- src/item/potion/Potion.java
+++ src/item/potion/Potion.java
@@ -6,10 +6,11 @@ import main.Game;
 public abstract class Potion extends Item {
 	
 	protected Game game;
-
-	public Potion(String name, Game game) {
+	int price;
+	public Potion(String name, Game game, int price) {
 		super(name);
 		this.game = game;
+		this.price = price;
 	}
 	
 	public abstract void apply ();
