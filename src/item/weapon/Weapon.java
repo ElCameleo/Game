ded9d2b26b2f5cbb5828e39683dbc5c1645418d1
Elred19 <65461188+Elred19@users.noreply.github.com>
2020-05-21 22:23:46 +0200

    Ajout durabilite + attaque

--- src/item/weapon/Weapon.java
+++ src/item/weapon/Weapon.java
@@ -5,10 +5,19 @@ import item.Item;
 public class Weapon extends Item {
 	
 	private float attack;
-
-	public Weapon(String name, float attack) {
+	private int durability;
+	public Weapon(String name, float attack, int durability) {
 		super(name);
 		this.attack = attack;
+		this.durability = durability;
 	}
-
+	public String getNmae() {
+		return(this.name);
+	}
+	public float getAttack() {
+		return(this.attack);
+	}
+	public int getDurability() {
+		return(this.durability);
+	}
 }
