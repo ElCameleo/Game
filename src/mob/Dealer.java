9c0a728b54d410297f4ce6e94fcf795b421e065b
Elred19 <65461188+Elred19@users.noreply.github.com>
2020-05-22 14:33:59 +0200

    Update Dealer.java

--- src/mob/Dealer.java
+++ src/mob/Dealer.java
@@ -1,24 +1,22 @@
+
 package mob;
 
 import java.util.ArrayList;
 
+import item.potion.*;
 import item.Item;
-import item.weapon.Hand;
-import item.weapon.Spear;
-import item.weapon.Sword;
 import javafx.scene.paint.Color;
 import main.Game;
 import utils.Vector;
 
 public class Dealer extends Mob {
-	
+	int cpt;	
+	int cpt2;
+	int cpt3;
 	private ArrayList<Item> store = new ArrayList<>();
-
 	public Dealer(Game game, Vector position) {
 		super(game, "DEALER", position, new Vector(1, 1), Color.LIGHTGOLDENRODYELLOW, 0f, 1);
-		store.add(new Hand());
-		store.add(new Spear());
-		store.add(new Sword());
+		FillStore();
 	}
 
 	@Override
@@ -26,8 +24,37 @@ public class Dealer extends Mob {
 		return new Vector(0, 0);
 	}
 	
-	public ArrayList<Item> getStore() {
-		return store;
+	public ArrayList<Item> getList() {
+		return (this.store);
 	}
+	
+	public void FillStore() {
+		LifePotion lp = new LifePotion(game,2);
+		MegaLifePotion mlp = new MegaLifePotion(game,5);
+		PowerPotion pp = new PowerPotion(game,3);
 
+		for(int i=0; i<5; i++) {
+			this.store.add(lp);
+			this.store.add(mlp);
+			this.store.add(pp);
+		}
+	}
+	
+	
+	public int[] itemCount(ArrayList<Item> store) {
+		for(Item obj : store) {
+			if(obj instanceof LifePotion) {
+				cpt++;
+			}
+			if(obj instanceof MegaLifePotion) {
+				cpt2++;
+			}
+			if(obj instanceof PowerPotion) {
+				cpt3++;
+			}
+		}
+		int[] compteur = {cpt,cpt2,cpt3};
+		return(compteur);
+	}
+	
 }
