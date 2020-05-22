95e7cb35baceb1c50e3e197f5e60fdb0ebf77489
Elred19 <65461188+Elred19@users.noreply.github.com>
2020-05-22 14:34:52 +0200

    Update LifePotion.java

--- src/item/potion/LifePotion.java
+++ src/item/potion/LifePotion.java
@@ -3,14 +3,14 @@ package item.potion;
 import main.Game;
 
 public class LifePotion extends Potion {
-
-	public LifePotion(Game game) {
-		super("LIFE_POTION", game);
+	int price;
+	public LifePotion(Game game,int price) {
+		super("LIFE_POTION", game, price);
+		this.price = price;
 	}
 
 	@Override
 	public void apply() {
 		game.player.life += 20;
 	}
-
 }
